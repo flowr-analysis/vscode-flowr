679052bc950053e7eb5978e916ca32dd49994c5a
Ellpeck <me@ellpeck.de>
2024-04-30 14:56:53 +0200

    clean up slice tests

--- src/test/slice.test.ts
+++ src/test/slice.test.ts
@@ -7,6 +7,17 @@ suite('slice', () => {
 		await activateExtension()
 	})
 
+	test('slice cursor', async() => {
+		await openTestFile('example.R', new vscode.Selection(7, 6, 7, 6))
+		const slice: string | undefined = await vscode.commands.executeCommand('vscode-flowr.slice.cursor')
+		assert.ok(slice)
+		assert.equal(slice, `
+product <- 1
+n <- 10
+for(i in 1:(n - 1)) product <- product * i
+			`.trim())
+	})
+
 	test('reconstruct cursor', async() => {
 		await openTestFile('example.R', new vscode.Selection(7, 6, 7, 6))
 		const newEditor: vscode.TextEditor | undefined = await vscode.commands.executeCommand('vscode-flowr.slice.cursor-reconstruct')
@@ -14,16 +25,6 @@ suite('slice', () => {
 		assert.equal(vscode.window.activeTextEditor, newEditor)
 		assert.ok(newEditor.document.fileName.startsWith('Untitled-'))
 		assert.equal(newEditor.document.getText(), `
-product <- 1
-n <- 10
-for(i in 1:(n - 1)) product <- product * i
-			`.trim())
-	})
-
-	test('slice cursor', async() => {
-		await openTestFile('example.R', new vscode.Selection(7, 6, 7, 6))
-		const slice: string | undefined = await vscode.commands.executeCommand('vscode-flowr.slice.cursor')
-		assert.equal(slice, `
 product <- 1
 n <- 10
 for(i in 1:(n - 1)) product <- product * i
--- src/test/test-util.ts
+++ src/test/test-util.ts
@@ -7,29 +7,17 @@ import * as path from 'path'
 export async function activateExtension(): Promise<void> {
 	const ext = vscode.extensions.getExtension('code-Inspect.vscode-flowr')
 	assert.notEqual(ext, undefined, 'extension not found')
-
 	await assert.doesNotReject(async() => {
 		await ext?.activate()
 	}, 'extension activation failed')
-
-	await sleep(1000)
 }
 
 export async function openTestFile(name: string, selection?: vscode.Selection): Promise<vscode.TextEditor> {
 	const file = path.resolve(__dirname, '..', '..', 'test-workspace', name)
-
 	const doc = await vscode.workspace.openTextDocument(file)
 	const editor = await vscode.window.showTextDocument(doc)
-
-	await sleep(1000)
-
 	if(selection) {
 		editor.selection = selection
 	}
-
 	return editor
-}
-
-export async function sleep(ms: number) {
-	return new Promise(resolve => setTimeout(resolve, ms))
 }
