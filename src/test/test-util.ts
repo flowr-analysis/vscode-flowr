28a07305084b998634a3fc1e56de4467f9de2092
Ellpeck <me@ellpeck.de>
2024-04-30 13:42:08 +0200

    sleep a bit between editor actions

--- src/test/test-util.ts
+++ src/test/test-util.ts
@@ -11,10 +11,22 @@ export async function activateExtension(): Promise<void> {
 	await assert.doesNotReject(async() => {
 		await ext?.activate()
 	}, 'extension activation failed')
+
+	// wait a bit to make sure everything has started fully
+	await sleep(1000)
 }
 
 export async function openTestFile(name: string): Promise<vscode.TextEditor> {
 	const file = path.resolve(__dirname, '..', '..', 'test-workspace', name)
 	const doc = await vscode.workspace.openTextDocument(file)
-	return await vscode.window.showTextDocument(doc)
+	const editor = await vscode.window.showTextDocument(doc)
+
+	// wait for text document to be opened
+	await sleep(1000)
+
+	return editor
+}
+
+export async function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms))
 }
