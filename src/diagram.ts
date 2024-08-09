ae60abe38f2cd501a0dffce7d0dda49eef319c23
Ellpeck <me@ellpeck.de>
2024-08-09 14:47:26 +0200

    note that

--- src/diagram.ts
+++ src/diagram.ts
@@ -33,8 +33,9 @@ export function registerDiagramCommands(context: vscode.ExtensionContext, output
 }
 
 function createWebview(id: string, name: string, mermaid: string, output: vscode.OutputChannel) : vscode.WebviewPanel | undefined {
+	// https://github.com/mermaid-js/mermaid/blob/47601ac311f7ad7aedfaf280d319d75434680622/packages/mermaid/src/mermaidAPI.ts#L315-L317
 	if(mermaid.length > mermaidMaxTextLength()){
-		void vscode.window.showErrorMessage('The diagram is too large to be displayed by Mermaid. You can find its code in the flowR output panel instead.')
+		void vscode.window.showErrorMessage('The diagram is too large to be displayed by Mermaid. You can find its code in the flowR output panel instead. Additionally, you can change the maximum diagram length in the extension settings.')
 		output.appendLine(mermaid)
 		return undefined
 	}
