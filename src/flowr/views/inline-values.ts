b48ef904a3db2ce8dae56635b3418445fd0ba6e0
Florian Sihler <florian.sihler@uni-ulm.de>
2025-05-28 22:22:00 +0200

    feat: improve on resolve value of variable

--- src/flowr/views/inline-values.ts
+++ src/flowr/views/inline-values.ts
@@ -1,13 +1,12 @@
 import * as vscode from 'vscode';
-import { PipelineOutput } from '@eagleoutice/flowr/core/steps/pipeline/pipeline';
-import { TREE_SITTER_DATAFLOW_PIPELINE, createDataflowPipeline } from '@eagleoutice/flowr/core/steps/pipeline/default-pipelines';
-import { TreeSitterExecutor } from '@eagleoutice/flowr/r-bridge/lang-4.x/tree-sitter/tree-sitter-executor';
-import { requestFromInput } from '@eagleoutice/flowr/r-bridge/retriever';
 import { NodeId } from '@eagleoutice/flowr/r-bridge/lang-4.x/ast/model/processing/node-id';
 import { VertexType } from '@eagleoutice/flowr/dataflow/graph/vertex';
 import { resolveIdToValue } from '@eagleoutice/flowr/dataflow/environments/resolve-by-name';
 import { RLogicalValue } from '@eagleoutice/flowr/r-bridge/lang-4.x/ast/model/nodes/r-logical';
 import { RNumberValue, RStringValue } from '@eagleoutice/flowr/r-bridge/lang-4.x/convert-values';
+import { getFlowrSession } from '../../extension';
+import { DataflowGraph } from '@eagleoutice/flowr/dataflow/graph/graph';
+import { NormalizedAst } from '@eagleoutice/flowr/r-bridge/lang-4.x/ast/model/processing/decorate';
 
 export function registerInlineHints(output: vscode.OutputChannel): vscode.Disposable {
 	return vscode.languages.registerInlayHintsProvider(
@@ -24,7 +23,8 @@ class FlowrInlayHintsProvider implements vscode.InlayHintsProvider {
 
 	// TODO: work with the server as well
 	// TODO: merge infrastructure with dependency viewer?
-	private analysisInfo: PipelineOutput<typeof TREE_SITTER_DATAFLOW_PIPELINE> | undefined;
+	private graphInfo: DataflowGraph | undefined;
+	private normalizeInfo: NormalizedAst | undefined;
 	// TODO: on update event etc.
 
 	constructor(output: vscode.OutputChannel) {
@@ -46,7 +46,6 @@ class FlowrInlayHintsProvider implements vscode.InlayHintsProvider {
 
 	private lastEditorContent: string | undefined;
 	async update(): Promise<void> {
-		this.output.appendLine('Updating inlay hints');
 		const active = vscode.window.activeTextEditor;
 		if(!active) {
 			return;
@@ -57,18 +56,22 @@ class FlowrInlayHintsProvider implements vscode.InlayHintsProvider {
 		}
 		this.lastEditorContent = content.trim();
 
-		this.analysisInfo = await createDataflowPipeline(new TreeSitterExecutor(), {
-			request: requestFromInput(content)
-		}).allRemainingSteps();
+		
+		const session = await getFlowrSession();
+		
+		const res = (await session.retrieveQuery(active.document, [{ type: 'dataflow' }, { type: 'normalized-ast' }]));
+		this.graphInfo = res.result.dataflow.graph;
+		this.normalizeInfo = res.result['normalized-ast'].normalized;
+		
 		this.updateEvent.fire();
 	}
 
 	private collectAllVariables(): Set<NodeId> {
-		if(!this.analysisInfo) {
+		if(!this.graphInfo) {
 			return new Set();
 		}
 		const variables = new Set<NodeId>();
-		for(const [v,info] of this.analysisInfo.dataflow.graph.vertices(true)) {
+		for(const [v,info] of this.graphInfo.vertices(true)) {
 			if(info.tag === VertexType.Use) {
 				variables.add(v);
 			}
@@ -77,16 +80,15 @@ class FlowrInlayHintsProvider implements vscode.InlayHintsProvider {
 	}
 
 	private getValuesForVariable(variable: NodeId): string[] {
-		if(!this.analysisInfo) {
+		if(!this.graphInfo || !this.normalizeInfo) {
 			return [];
 		}
-		const values = resolveIdToValue(variable, { graph: this.analysisInfo.dataflow.graph, full: true, idMap: this.analysisInfo.normalize.idMap });
-
+		const values = resolveIdToValue(variable, { graph: this.graphInfo, full: true, idMap: this.normalizeInfo.idMap });
 		return values?.map(unwrapRValue).filter(isNotUndefined) ?? [];
 	}
 
 	provideInlayHints(document: vscode.TextDocument, range: vscode.Range, token: vscode.CancellationToken): vscode.ProviderResult<vscode.InlayHint[]> {
-		if(!this.analysisInfo) {
+		if(!this.graphInfo || !this.normalizeInfo) {
 			return [];
 		}
 		// TODO: respect hints
@@ -97,16 +99,18 @@ class FlowrInlayHintsProvider implements vscode.InlayHintsProvider {
 			if(values.length === 0) {
 				continue;
 			}
-			const loc = this.analysisInfo.normalize.idMap.get(variable);
+			const loc = this.normalizeInfo.idMap?.get(variable);
 			if(!loc?.location) {
 				continue;
 			}
 			const vals = values.join(' | ');
+			const position = new vscode.Position(loc.location[0] - 1, loc.location[1]);
 			results.push({
 				label: `: ${vals}`,
-				kind: vscode.InlayHintKind.Type,
-				position: new vscode.Position(loc.location[2], loc.location[3] - 1),
-				paddingLeft: true
+				tooltip: 'Values: ' + vals,
+				kind: vscode.InlayHintKind.Parameter,
+				position,
+				paddingLeft:	 true
 			})
 		}
 
