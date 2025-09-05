0ad2a4178befc341f54b4750076a66e19c3b24ba
Ellpeck <me@ellpeck.de>
2025-09-05 16:26:21 +0200

    remove test code

--- example/example-dependencies.R
+++ example/example-dependencies.R
@@ -5,14 +5,8 @@ data %>%
    dplyr::group_by(group) %>%
    dplyr::do(anova(lm(value ~ treatment, data = .))) -> anova_results
 
-read.csv("hello")
-read.csv(path.expand("blah"))
-
 write.csv(anova_results, "anova_results.csv")
 
 library(ggplot2)
 ggplot(mpg, aes(displ, hwy, colour = class)) +
   geom_point()
-
-sink("./test.txt")
-cat("Hello")
